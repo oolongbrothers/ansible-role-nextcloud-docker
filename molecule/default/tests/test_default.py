592633243ab1da574c682fa5ba390c9c89c1e469
Christopher Torgalson <manager@bedlamhotel.com>
2019-06-06 10:54:10 +0200

    Adds multiple tests to role
    - Tests container existence and status.
    - Tests install status of nextcloud app.
    - Tests system encryption status.
    - Tests encryption module status.
    - add-basic-tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,13 +2,51 @@ import os
 
 import testinfra.utils.ansible_runner
 
+import pytest
+
+import re
+
+import json
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize('container', [
+    'nextcloud-app',
+    'nextcloud-mariadb',
+    'nextcloud-nginx',
+])
+def test_container_status(host, container):
+    c = 'docker ps --all --quiet --format "{{.Names}}" --filter status=running'
+    r = host.run(c)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert container in r.stdout
+
+
+def test_nextcloud_status(host):
+    c = ('docker exec --user www-data nextcloud-app '
+         'php occ status --output=json')
+    r = host.run(c)
+    n = json.loads(r.stdout)
+
+    assert n['enabled']
+
+
+def test_encryption_status(host):
+    c = ('docker exec --user www-data nextcloud-app '
+         'php occ encryption:status --output=json')
+    r = host.run(c)
+    e = json.loads(r.stdout)
+
+    assert e['enabled']
+
+
+def test_encryption_module_status(host):
+    c = ('docker exec --user www-data nextcloud-app '
+         'php occ app:list --output=json')
+    r = host.run(c)
+    e = json.loads(r.stdout)
+    p = re.compile('\\d+\\.\\d+\\.\\d+')
+
+    assert p.match(e['enabled']['encryption'])
